e600db0c67ee3151767e172852bba256fa2d056d
kisinja <kisinjaelvis@gmail.com>
2025-08-14 17:25:10 +0300

    fixed sitemap config file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,18 +1,18 @@
-import { IConfig, ISitemapField } from "next-sitemap";
 import { allBlogs } from "./.contentlayer/generated"; // adjust path
-import siteMetaData from "@/utils/siteMetaData"; // adjust path
+import { siteUrl as _siteUrl } from "./utils/siteMetaData"; // adjust path
 
-const config: IConfig = {
-  siteUrl: siteMetaData.siteUrl,
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: _siteUrl,
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "daily",
   priority: 0.7,
 
   // Add dynamic blog pages
-  additionalPaths: async (): Promise<ISitemapField[]> => {
+  additionalPaths: async () => {
     return allBlogs.map((blog) => ({
-      loc: `${siteMetaData.siteUrl}${blog.url}`,
+      loc: `${_siteUrl}${blog.url}`,
       lastmod: new Date(blog.updatedAt).toISOString(),
       changefreq: "weekly",
       priority: 0.8,
